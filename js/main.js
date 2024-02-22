24a43bf5a559489e22bb9da2556bb0a766df49b1
GonzaBottone <bottonegonza@gmail.com>
2024-02-22 02:02:59 -0300

    gonza

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ letras.forEach(letra => {
   span.classList.add('letra-hover');
   texto.appendChild(span);
 });
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
